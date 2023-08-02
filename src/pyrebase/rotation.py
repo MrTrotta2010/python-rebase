c20aeff320fd20e347bba0d39f2176a19e723036
Tiago Trotta <mrtrotta2010@gmail.com>
2023-08-01 23:14:50 -0300

    Atualiza Rotation

--- src/pyrebase/rotation.py
+++ src/pyrebase/rotation.py
@@ -1,8 +1,11 @@
 class Rotation:
-  def __init__(self, x, y, z = None):
-    self.x = x
-    self.y = y
-    self.z = z
+  def __init__(self, x: int | float = None, y: int | float = None, z: int | float = None):
+    self.x = x or 0
+    self.y = y or 0
+    self.z = z or 0
+
+  def __eq__(self, other):
+    return isinstance(other, Rotation) and self.x == other.x and self.y == other.y and self.z == other.z
 
   def __str__(self):
-    return f'[{self.x}, {self.y}{("" if self.z is None else f", {self.z}")}]'
+    return f'[{round(self.x, 4)}, {round(self.y, 4)}, {round(self.z, 4)}]'
