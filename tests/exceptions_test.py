8d6d37c36ff4561a2ebac9d5103fb31a5356d480
Tiago Trotta <mrtrotta2010@gmail.com>
2023-08-04 16:44:56 -0300

    Condesa testes de exceções

--- tests/exceptions_test.py
+++ tests/exceptions_test.py
@@ -5,26 +5,21 @@ from src.pyrebase.mismatched_articulations_error import MismatchedArticulationsE
 from src.pyrebase.repeated_articulation_error import RepeatedArticulationError
 
 class TestExceptions:
-  def test_missing_attribute_error(self):
+  def test_exceptions(self):
     with pytest.raises(MissingAttributeError):
       raise MissingAttributeError('attribute')
 
-  def test_missing_attribute_error_message(self):
-    e = MissingAttributeError('attribute')
-    assert str(e) == "Essential attribute 'attribute' is missing from request"
-
-  def test_mismatched_articulation_error(self):
     with pytest.raises(MismatchedArticulationsError):
       raise MismatchedArticulationsError(['a1'], ['a2'])
 
-  def test_mismatched_articulation_error_message(self):
-    e = MismatchedArticulationsError(['a1'], ['a2'])
-    assert str(e) == "Articulation lists do not match: ['a1'] and ['a2']"
-
-  def test_repeated_articulation_error(self):
     with pytest.raises(RepeatedArticulationError):
       raise RepeatedArticulationError('a1', ['a1', 'a1'])
 
-  def test_repeated_articulation_error_message(self):
-    e = RepeatedArticulationError('a1', ['a1', 'a1'])
-    assert str(e) == "Duplicate articulation 'a1' in list ['a1', 'a1']"
+  def test_messages(self):
+    missing_e = MissingAttributeError('attribute')
+    mismatched_e = MismatchedArticulationsError(['a1'], ['a2'])
+    repeated_e = RepeatedArticulationError('a1', ['a1', 'a1'])
+
+    assert str(missing_e) == "Essential attribute 'attribute' is missing from request"
+    assert str(mismatched_e) == "Articulation lists do not match: ['a1'] and ['a2']"
+    assert str(repeated_e) == "Duplicate articulation 'a1' in list ['a1', 'a1']"
